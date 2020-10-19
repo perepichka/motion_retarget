5235ac60c62fa7298a9473dbe3fc2cc1c69883f8
Maksym Perepichka <max@maxedoutstudio.com>
2020-10-19 19:22:10 -0400

    Initial commits for data, models and visualize

---
+++ data.py
@@ -0,0 +1,11 @@
+"""Module for processing animation data."""
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+
+if __name__ == '__main__':
+    pass
