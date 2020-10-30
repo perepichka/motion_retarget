f54ec784b4669c4233da5d13abf68e0c4cd41e0d
Maksym Perepichka <max@maxedoutstudio.com>
2020-10-29 20:53:37 -0400

    Fixed data saving/loading

--- data.py
+++ data.py
@@ -2,6 +2,7 @@
 import sys, os, glob
 
 from tqdm import tqdm
+import logging
 
 import numpy as np
 import torch
@@ -32,7 +33,6 @@ DATASETBASE_ARGUMENTS = {
     'window_size': DEFAULT_WINDOW_SIZE,
     'window_interval': DEFAULT_INTERVAL,
     'visualize_loading': False,
-    'save': True,
 }
 
 
@@ -54,6 +54,9 @@ class _AnimDatasetBase(Dataset):
 
         # Set up some parameters
         self.num_joints = 0
+        self.num_frames = 0
+        self.num_anims = 0
+
         self._frames = None
         self._anim_ranges = []
         self._anim_names = []
@@ -72,14 +75,24 @@ class _AnimDatasetBase(Dataset):
 
         stored_anims = []
 
+        logging.info('Parsing animation files...')
         for character in tqdm(characters):
 
-            anims = sorted(os.listdir(os.path.join(self.path, character)))
+            char_path = os.path.join(self.path, character)
+
+            if not os.path.isdir(char_path):
+                continue
+
+            anims = sorted(os.listdir(char_path))
 
             for anim_name in anims:
+
                 anim_path = os.path.join(
                     self.path, character, anim_name, 
                 )
+
+                if not os.path.isdir(anim_path):
+                    continue
 
                 takes = sorted(os.listdir(anim_path))
 
@@ -104,25 +117,37 @@ class _AnimDatasetBase(Dataset):
                     if self.num_joints == 0:
                         self.num_joints = anim.shape[-2]
 
-                    # Stores animation
-                    if self._frames is None:
-                        self._frames = torch.empty([0, self.num_joints, 3])
+                    self.num_frames += anim.shape[0]
+                    self.num_anims += 1
 
-                    self._frames = torch.cat(
-                        [self._frames, torch.from_numpy(anim)], dim=0)
+                    stored_anims.append(anim)
                     self._anim_ranges.append(
-                        (len(self._frames)-1,
-                        len(self._frames)-1+anim.shape[0])
+                        (self.num_frames-1+anim.shape[0])
                     )
                     self._anim_names.append(
                         '{}_{}'.format(character, anim_name)
                     )
 
 
+        # Makes sure we have loaded animations           
+        if len(stored_anims) == 0:
+            raise Exception('Failed to load any frames!')
+
+        # Resizes everything
+        self._frames = torch.empty([self.num_frames, self.num_joints, 3])
+        
+        curr_index = 0
+        logging.info('Storing animation files in memory...')
+        for anim in stored_anims:
+            # Stores animation
+            self._frames[curr_index: curr_index+anim.shape[0]] = torch.from_numpy(anim)
+            curr_index += anim.shape[0]
+            
+
         if self._frames is None or self._frames.shape[0] == 0:
             raise Exception('Failed to load any frames!')
 
-        logging.info('Finished loading {} frames').format(self._frames.shape[0])
+        logging.info('Finished loading {} frames'.format(self._frames.shape[0]))
 
 
     def _load_video(self):
@@ -155,14 +180,20 @@ class AnimDataset(_AnimDatasetBase):
 
     def save(self, path=None):
         """Serializes the loaded dataset."""
+
         if path is None:
-            path = self.path
+            path = os.path.join(self.path, 'ds.pt')
+
+        if self.num_joints == 0 or self.num_frames == 0:
+            logging.warning('Attempting to save empty database!')
+
         logging.info('Saving model to {}'.format(path))
+
         torch.save({
-            'num_joints': self.num_joints,
-            '_frames': self.num_joints,
-            '_anim_ranges': self.num_joints,
-            '_anim_names': self.num_joints,
+            #'num_joints': self.num_joints,
+            '_frames': self._frames,
+            #'_anim_ranges': self._anim_ranges,
+            #'_anim_names': self._anim_names,
         }, path)
 
     def load(self, path=None):
@@ -171,7 +202,9 @@ class AnimDataset(_AnimDatasetBase):
         logging.info('Loading model from {}'.format(path))
         params = torch.load(path)
         for k,v in params.items():
-            setattr(self, key, value)
+            setattr(self, k, v)
+        self.num_joints = self._frames.shape[1]
+        self.num_frames = self._frames.shape[0]
 
 
 
@@ -191,5 +224,6 @@ class AnimDatasetWindowed(_AnimDatasetBase):
 
 if __name__ == '__main__':
     ds = AnimDataset()
-    ds.process()
-    ds.save()
+    ds.load()
+    #ds.process()
+    ds.save('./data/mixamo/36_800_24/train/tst.pt')
