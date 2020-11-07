95254c535c07dcd2995b554abc1837ac0f419e27
Maksym Perepichka <max@maxedoutstudio.com>
2020-11-07 17:44:29 -0500

    Added config loader

---
+++ utils.py
@@ -0,0 +1,23 @@
+"""Module for general utilities."""
+import sys, os, glob
+
+import logging
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+
+import yaml
+
+
+def get_config(config_path):
+    with open(config_path, 'r') as stream:
+        config = yaml.load(stream, Loader=yaml.SafeLoader)
+    config = edict(config)
+    _, config_filename = os.path.split(config_path)
+    config_name, _ = os.path.splitext(config_filename)
+    config.name = config_name
+    return config
